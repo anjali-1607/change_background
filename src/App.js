0d8a55ba09cde1850da5d5778b464b89d53b2876
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-19 12:47:08 +0530

    color changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 import { Button } from "semantic-ui-react";
 
 export default function App() {
+  const [color, setColor] = useState("blue");
+  const change = () => {
+    setColor((document.body.style.backgroundColor = "red"));
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "pink";
+  }, []);
   return (
-    <div>
-      <Button>Change Color</Button>
+    <div
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        margin: "19em 0em 0em 0em",
+      }}>
+      <Button size="big" onClick={change}>
+        Change background
+      </Button>
     </div>
   );
 }
